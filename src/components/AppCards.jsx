64227494882b3ecce1f85cdbd414d54483bf9e1b
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-11-30 09:29:19 +0100

    add-components-cards

---
+++ src/components/AppCards.jsx
@@ -0,0 +1,34 @@
+import { useGlobalContext } from '../contexts/GlobalContext';
+import dataFlags from '../data/dataFlags';
+import AppStars from './AppStars';
+
+import Flag from 'react-world-flags'
+export default function AppCards() {
+    const { task } = useGlobalContext();
+
+
+    return (
+        <>
+
+            {task.length > 0 ? (
+                task.map(post => (
+                    <div className=" col-3 p-3" key={post.id}>
+                        <div className="card">
+                            <img src={`https://image.tmdb.org/t/p/w500/${post.poster_path}`} alt="" />
+                            <ul className='card-hover bg-black text-white'>
+                                <li>{post.title}</li>
+                                <li>{post.original_title}</li>
+                                <li> Languages: <Flag code={dataFlags[post.original_language]} style={{ height: 20 }} /></li>
+                                <li>{AppStars(post.vote_average)}</li>
+                                <li>{post.original_name}</li>
+
+                            </ul>
+                        </div>
+                    </div>
+                ))
+            ) : (<p>No results</p>
+            )}
+
+        </>
+    )
+}
